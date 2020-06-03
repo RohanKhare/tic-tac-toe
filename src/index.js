132d2b8c307895cc21b9a9812c556bd323584744
RohanKhare <53505979+RohanKhare@users.noreply.github.com>
2020-06-03 10:04:30 +0530

    new src folder

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './Components/Game';
+import './Components/Game.css';
+
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
