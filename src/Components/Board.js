619aaaaffc8a116698865e2f0ed61da4dc39c998
RohanKhare <53505979+RohanKhare@users.noreply.github.com>
2020-06-08 18:40:03 +0530

    Code to calculate winner

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -20,8 +20,10 @@ class Board extends React.Component{
                  />       
        }
        handleOnClick(i){
-         //alert("Hello!! " + i)
          const squares = this.state.squares.slice();
+         if(this.calculateWinner(squares) || squares[i]){
+           return;
+         }
          squares[i] = (this.state.xIsNext ? 'X' : 'O')
          this.setState({
           squares : squares,
@@ -41,7 +43,13 @@ class Board extends React.Component{
         }
      
        render(){
-        let status = 'Next player : '+ (this.state.xIsNext ? 'X' : 'O')
+        const winner = this.calculateWinner(this.state.squares);
+         let status;
+        if(winner){
+          status = 'Winner : ' +winner;
+        }else {
+          status = 'Next player : '+ (this.state.xIsNext ? 'X' : 'O');
+        }
         let counter = 0;
          let addBy = []
          for(let j=0;j<this.state.dimension;j++){
@@ -57,6 +65,79 @@ class Board extends React.Component{
              </div>
            </div>
          );
+       }
+
+
+       calculateWinner(squares){
+          if(this.state.dimension === 3){
+            const lines=[
+              [0,1,2],
+              [3,4,5],
+              [6,7,8],
+              [0,3,6],
+              [1,4,7],
+              [2,5,8],
+              [0,4,8],
+              [2,4,6]
+            ];
+
+            for(let i=0; i<lines.length;i++){
+              const [a, b, c] = lines[i];
+              if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]){
+                return squares[a];
+              }
+            }
+            return null;
+          }
+
+          if(this.state.dimension === 5){
+            const lines=[
+              [0,1,2,3,4],
+              [5,6,7,8,9],
+              [10,11,12,13,14],
+              [15,16,17,18,19],
+              [20,21,22,23,24],
+              [0,5,10,15,20],
+              [1,6,11,16,21],
+              [2,7,12,17,22],
+              [3,8,13,18,23],
+              [4,9,14,19,24],
+              [0,6,12,18,24],
+              [4,8,12,16,20]
+            ];
+
+            for(let i=0; i<lines.length;i++){
+              const [a, b, c, d, e] = lines[i];
+              if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c] && squares[a]===squares[d] && squares[a]===squares[e]){
+                return squares[a];
+              }
+            }
+            return null;
+          }
+          // if(this.state.dimension === 7){
+          //   const lines=[
+          //     [0,1,2,3,4,5,6],
+          //     [7,8,9,10,11,12,13],
+          //     [14,15,16,17,18,19,20],
+          //     [],
+          //     [21,22,23,24],
+          //     [0,5,10,15,20],
+          //     [1,6,11,16,21],
+          //     [2,7,12,17,22],
+          //     [3,8,13,18,23],
+          //     [4,9,14,19,24],
+          //     [0,6,12,18,24],
+          //     [4,8,12,16,20]
+          //   ];
+
+          //   for(let i=0; i<lines.length;i++){
+          //     const [a, b, c, d, e] = lines[i];
+          //     if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c] && squares[a]===squares[d] && squares[a]===squares[e]){
+          //       return squares[a];
+          //     }
+          //   }
+          //   return null;
+          // }
        }
 }
 export default Board;
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -12,7 +12,7 @@ class Game extends React.Component{
       return(
         <div className="game"> 
           <div className="game-board">
-              <Board dimension={7} />
+              <Board dimension={5} />
           </div>
           <div className="game-info">
             <div></div>
