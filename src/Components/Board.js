cfaa653c4bc8ce4fcc39413c561c65efce2d1931
RohanKhare <53505979+RohanKhare@users.noreply.github.com>
2020-06-09 09:41:53 +0530

    Complete tic-tac-toe for 3/5/7 dimension

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -16,6 +16,7 @@ class Board extends React.Component{
     }
 
       renderSqaure(i){
+        //
         return <Square value={this.state.squares[i]} onClick={()=>this.handleOnClick(i)}
                  />       
        }
@@ -30,9 +31,7 @@ class Board extends React.Component{
           xIsNext : !this.state.xIsNext
          });
        }
-       
-     
-       
+             
         incrementByRow(cnt, counter){
           let temp = []          
           for(let i=0;i<cnt;i++){
@@ -114,30 +113,36 @@ class Board extends React.Component{
             }
             return null;
           }
-          // if(this.state.dimension === 7){
-          //   const lines=[
-          //     [0,1,2,3,4,5,6],
-          //     [7,8,9,10,11,12,13],
-          //     [14,15,16,17,18,19,20],
-          //     [],
-          //     [21,22,23,24],
-          //     [0,5,10,15,20],
-          //     [1,6,11,16,21],
-          //     [2,7,12,17,22],
-          //     [3,8,13,18,23],
-          //     [4,9,14,19,24],
-          //     [0,6,12,18,24],
-          //     [4,8,12,16,20]
-          //   ];
+          if(this.state.dimension === 7){
+            const lines=[
+              [0, 1, 2, 3, 4, 5, 6],
+              [7, 8, 9, 10,11,12,13],
+              [14,15,16,17,18,19,20],
+              [21,22,23,24,25,26,27],
+              [28,29,30,31,32,33,34],
+              [35,36,37,38,39,40,41],
+              [42,43,44,45,46,47,48],
 
-          //   for(let i=0; i<lines.length;i++){
-          //     const [a, b, c, d, e] = lines[i];
-          //     if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c] && squares[a]===squares[d] && squares[a]===squares[e]){
-          //       return squares[a];
-          //     }
-          //   }
-          //   return null;
-          // }
+              [0,7,14,21,28,35,42],
+              [1,8,15,22,29,36,43],
+              [2,9,16,23,30,37,44],
+              [3,10,17,24,31,38,45],
+              [4,11,18,25,32,39,46],
+              [5,12,19,26,33,40,47],
+              [6,13,20,27,34,41,48],
+              
+              [0,8,16,24,32,40,48],
+              [6,12,28,24,30,36,42]
+            ];
+
+            for(let i=0; i<lines.length;i++){
+              const [a, b, c, d, e, f, g] = lines[i];
+              if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c] && squares[a]===squares[d] && squares[a]===squares[e] && squares[a]===squares[f] && squares[a]===squares[g]){
+                return squares[a];
+              }
+            }
+            return null;
+          }
        }
 }
 export default Board;
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -12,7 +12,7 @@ class Game extends React.Component{
       return(
         <div className="game"> 
           <div className="game-board">
-              <Board dimension={5} />
+              <Board dimension={7} />
           </div>
           <div className="game-info">
             <div></div>
